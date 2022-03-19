db37e3e574b63e018ba1a049728a12164fde4d0b
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-03-19 23:27:33 +0100

    index

---
+++ index.js
@@ -0,0 +1,19 @@
+const Mustache = require('mustache');
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString());
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+async function action() {
+  await generateReadMe();
+}
+
+action();
